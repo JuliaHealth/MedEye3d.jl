01657b8bcaf20380f382ec58bf1683acebb4b4cc
DrWatson <no@mail>
2021-05-23 18:47:11 +0200

    up

---
+++ scripts/displaying/mainDisplay.jl
@@ -0,0 +1,20 @@
+
+using DrWatson
+using FileIO
+using loadDicomFromDisk
+@quickactivate "Probabilistic medical segmentation"
+
+```@doc
+
+
+```
+module mainDisplay 
+#wload(filename)
+using ITK
+dcms = load_dicom("C:\\Users\\jakub\\OneDrive\\Documents\\GitHub\\probabilisticSegmentation\\Probabilistic medical segmentation\\data\\liverSegm\\training-scans\\scan\\liver-orig001")
+
+
+
+end
+
+
--- scripts/loadData/loadDicomFromDisk.jl
+++ scripts/loadData/loadDicomFromDisk.jl
@@ -1,6 +1,35 @@
 using DrWatson
+using Dicom
 @quickactivate "Probabilistic medical segmentation"
 
 ```@doc
-The data from 
+Loading Dicom data  into julia Arrays
 ```
+module loadDicomFromDisk
+
+function load_dicom(dir)
+    dcms = dcmdir_parse(dir)
+    loaded_dcms = Dict()
+    # 'dcms' could contain data for different series, so we have to filter by series
+    unique_series = unique([dcm.SeriesInstanceUID for dcm in dcms])
+    for (idx, series) in enumerate(unique_series)
+        dcms_in_series = filter(dcm -> dcm.SeriesInstanceUID == series, dcms)
+        pixeldata = extract_pixeldata(dcms_in_series)
+        loaded_dcms[idx] = (; pixeldata = pixeldata, dcms = dcms_in_series)
+    end
+    return loaded_dcms
+end
+
+function extract_pixeldata(dcm_array)
+    if length(dcm_array) == 1
+        return only(dcm_array).PixelData
+    else
+        return cat([dcm.PixelData for dcm in dcm_array]...; dims = 3)
+    end
+
+
+end
+
+
+
+end # module
