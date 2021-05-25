6ebd91355e9a20b722ee62879d0420cdc65d2b80
DrWatson <no@mail>
2021-05-25 17:09:44 +0200

    up

--- scripts/displaying/mainDisplay.jl
+++ scripts/displaying/mainDisplay.jl
@@ -13,8 +13,8 @@ module mainDisplay
 using ITK
 dcms = load_dicom("C:\\Users\\jakub\\OneDrive\\Documents\\GitHub\\probabilisticSegmentation\\Probabilistic medical segmentation\\data\\liverSegm\\training-scans\\scan\\liver-orig001")
 
-
-
+datadir("foo", "test.jld2")
+Probabilistic medical segmentation\data\hdf5Main\mainHdDataBaseLiver07.hdf5
 end
 
 
