bb1b6b5fccfdfd17e2aa3fab7cb6ed4255ad884d
DrWatson <no@mail>
2021-05-29 11:21:09 +0200

    up

--- scripts/display/mainDisplay.jl
+++ scripts/display/mainDisplay.jl
@@ -2,7 +2,6 @@
 ```@doc
 functions responsible for displaying medical image Data
 ```
-
 using DrWatson
 @quickactivate "Probabilistic medical segmentation"
 using GLMakie
@@ -10,57 +9,23 @@ using GLMakie
 #using AbstractPlotting
 using Plots 
 using GeometryBasics
-```@doc
-simple display of single image - only in transverse plane
-```
-function singleCtScanDisplay(arr ::Array{Number, 3}) 
-  
-  slicesNumb =size(arr)[3]
-  fig = Figure()
-  sl_x = Slider(fig[2, 1], range = 1:1: slicesNumb , startvalue = convert(Int32,slicesNumb/2 ) )
-  ax = Axis(fig[1, 1])
-  hm = heatmap!(ax, lift(idx-> arr[:,:, convert(Int32,idx)], sl_x.value) ,colormap = :grays)
-  Colorbar(fig[1, 2], hm)
+using GeometricalPredicates
+using Distances
 
 
-end
-
-#const arrr= getExample()
-
-#display mouseposition inspired by   https://github.com/JuliaPlots/Makie.jl/issues/810
-function indicatorC(ax::Axis,dims::Tuple{Int64, Int64, Int64},sc::Scene,maskArr::Observable{BitArray{3}},sliceNumb::Observable{Any})
-  register_interaction!(ax, :indicator) do event::Makie.MouseEvent, axis
-  if event.type === MouseEventTypes.leftclick
-    position=(0,0)
-    size=1
-    #position from top left corner 
-    compBoxOrigin = to_world(sc,ax1.layoutobservables.computedbbox[].origin)
-    xMouse= to_world(sc,event.data)[1]
-    yMouse= to_world(sc,event.data)[2]
-    #data about height and width in layout
-      #compBoxDims = ax1.layoutobservables.computedbbox[].widths
-    compBoxWidth = 510 #Makie.to_world(sc,compBoxDims)[1] -compBoxOrigin[1]
-    compBoxHeight = 510 #Makie.to_world(sc,compBoxDims)[2] -compBoxOrigin[1]
-    #image dimensions - number of pixels  from medical image for example ct scan
-    pixelsNumbInX =dims[1]
-    pixelsNumbInY =dims[2]
-    #calculating over which image pixel we are
-    calculatedXpixel = round( (xMouse/compBoxWidth)*pixelsNumbInX )
-    calculatedYpixel = round(  (yMouse/compBoxHeight)*pixelsNumbInY )
-    maskArrB = maskArr[]
-    maskArrB[ convert(Int32,calculatedXpixel), convert(Int32,calculatedYpixel), convert(Int32,sliceNumb[]) ]=1
-    maskArr[] = maskArrB
-    print("xMouse: $(xMouse)  yMouse: $(yMouse)   compBoxWidth: $(compBoxWidth)  compBoxHeight: $(compBoxHeight)   calculatedXpixel: $(calculatedXpixel)  calculatedYpixel: $(calculatedYpixel)      pixelsNumbInX  $(pixelsNumbInX)         ") 
-    return false
-  end
-  end
-end
-
-# dimenstion of the primary image for example CT scan
 imageDim = size(arrr)
 maskArr = Observable(BitArray(undef, imageDim))
-# number of slices 
-slicesNumb =imageDim[3]
+singleCtScanDisplay(arrr, maskArr)
+
+
+```@doc
+simple display of single image - only in transverse plane we are adding also a mask  that 
+arrr - main 3 dimensional data representing medical image for example in case of CT each voxel represents value of X ray attenuation
+```
+function singleCtScanDisplay(arrr ::Array{Number, 3}, maskArr::Observable{BitArray{3}} ) 
+  
+imageDim = size(arrr) # dimenstion of the primary image for example CT scan
+slicesNumb =imageDim[3] # number of slices 
 
 #defining layout variables
 scene, layout = Makie.layoutscene(resolution = (600, 400))
@@ -71,47 +36,114 @@ ax2 = layout[1, 1] = Makie.Axis(scene, backgroundcolor = :transparent)
 sl_x =layout[2, 1]= Makie.Slider(scene, range = 1:1: slicesNumb , startvalue = slicesNumb/2 )
 sliderXVal = sl_x.value
 
+
 #color maps
 cmwhite = cgrad(range(RGBA(10,10,10,0.01), stop=RGBA(0,0,255,0.4), length=10000));
-#cmwhite = cgrad(ColorScheme([RGBA(10,10, 10,0.1),RGBA(220,0, 0,0.8)]))
 
-    #cmwhite = cgrad(range(HSLA(0,0,1,0), stop=HSLA(0,0,1,1), length=100));
+####heatmaps
 
-#heatmaps
 #main heatmap that holds for example Ct scan
 currentSliceMain = Makie.@lift(arrr[:,:, convert(Int32,$sliderXVal)])
 hm = Makie.heatmap!(ax1, currentSliceMain ,colormap = :grays) 
 
 #helper heatmap designed to respond to both changes in slider and changes in the bit matrix
 currentSliceMask = Makie.@lift($maskArr[:,:, convert(Int32,$sliderXVal)])
-
 hmB = Makie.heatmap!(ax1, currentSliceMask ,colormap = cmwhite) 
 
-
-# #formatting
-# hidedecorations!(ax1, grid = false)
-# hidedecorations!(ax2, grid = false)
-
-#display mouseposition  https://github.com/JuliaPlots/Makie.jl/issues/810
-
+#adding ability to be able to add information to mask  where we clicked so in casse of mit matrix we will set the point where we clicked to 1 
 indicatorC(ax1,imageDim,scene,maskArr,sliderXVal)
-#indicatorC(layout,imageDim)
-
-
 
 #displaying
+colorB = layout[1,2]= Colorbar(scene, hm)
 Makie.translate!(hmB, Vec3f0(0,0,5))  
 scene
 
+end
+
+```@doc
+inspired by   https://github.com/JuliaPlots/Makie.jl/issues/810
+Generaly thanks to this function  the viewer is able to respond to clicking on the slices and records it in the supplied 3 dimensional bitarray
+  ax - Axis which store our heatmap slices which we want to observe wheather user clicked on them and where
+  dims - dimensions of  main image for example CT
+  sc - Scene where our axis is
+  maskArr - the 3 dimensional bit array  that has exactly the same dimensions as main Array storing image 
+  sliceNumb - represents on what slide we are on currently on - ussually it just give information from slider 
+```
+function indicatorC(ax::Axis,dims::Tuple{Int64, Int64, Int64},sc::Scene,maskArr::Observable{BitArray{3}},sliceNumb::Observable{Any})
+  register_interaction!(ax, :indicator) do event::Makie.MouseEvent, axis
+  if event.type === MouseEventTypes.leftclick
+    #position from top left corner 
+    xMouse= to_world(sc,event.data)[1]
+    yMouse= to_world(sc,event.data)[2]
+    #data about height and width in layout
+    compBoxWidth = 510 
+    compBoxHeight = 510 
+    #image dimensions - number of pixels  from medical image for example ct scan
+    pixelsNumbInX =dims[1]
+    pixelsNumbInY =dims[2]
+    #calculating over which image pixel we are
+    calculatedXpixel =  (xMouse/compBoxWidth)*pixelsNumbInX 
+    calculatedYpixel =   (yMouse/compBoxHeight)*pixelsNumbInY 
+    sliceNumbConv = sliceNumb[] 
+    #appropriately modyfing wanted pixels in mask array
+    markMaskArrayPatch( maskArr ,Point3D(calculatedXpixel, calculatedYpixel, sliceNumbConv  ))
 
 
 
+    #print("xMouse: $(xMouse)  yMouse: $(yMouse)   compBoxWidth: $(compBoxWidth)  compBoxHeight: $(compBoxHeight)   calculatedXpixel: $(calculatedXpixel)  calculatedYpixel: $(calculatedYpixel)      pixelsNumbInX  $(pixelsNumbInX)         ") 
+  return true
+  end
+end
+end
 
-# function indicatorMy(ob)
-#   print( "aaaa")
-# end
-
-# register_interaction!(indicatorMy,ax1, :indicatorMyInter) 
 
 
+```@doc
+  maskArr - the 3 dimensional bit array  that has exactly the same dimensions as main Array storing image 
+  point - point around which we want to modify the bitArray from 0 to 1
+
+```
+function markMaskArrayPatch(maskArr::Observable{BitArray{3}}, point::Point3D)
+  calculatedXpixel= convert(Int32, round(GeometricalPredicates.getx(point) ))
+  calculatedYpixel= convert(Int32,round(GeometricalPredicates.gety(point) ))
+  sliceNumbConv= convert(Int32,round(GeometricalPredicates.getz(point) ))
+
+  maskArrB = maskArr[]
+    
+  maskArrB[calculatedXpixel, calculatedYpixel, sliceNumbConv]=1
+  maskArrB[calculatedXpixel+1, calculatedYpixel+1, sliceNumbConv]=1
+  maskArrB[calculatedXpixel+1, calculatedYpixel-1, sliceNumbConv]=1
+  maskArrB[calculatedXpixel-1, calculatedYpixel+1, sliceNumbConv]=1
+  maskArrB[calculatedXpixel-1, calculatedYpixel-1, sliceNumbConv]=1
+
+
+  maskArr[] = maskArrB
+
+
+
+end
+
+
+```@doc
+  we are looking in given 3 dimensional array closest points in distnace not bigger than dist measure in euclidean distance
+
+  1)create a 3 dimensional matrix  that will have edge equal to distance
+  2)take all cartesian coordinates from it
+  3)add to those cartesian
+
+  arr - the 3 dimensional bit array  that has exactly the same dimensions as main Array storing image 
+  point - point around which we want to modify the bitArray from 0 to 1
+  dist - maximal euclidean distance to which we want  to apply some operations
+  returns series of cartesian coordinates of points closest to given point using euclidean distance measure 
+```
+function getIndexesclosestToPoint(arr::BitArray{3}, point::Point3D) 
+  calculatedXpixel= convert(Int32, round(GeometricalPredicates.getx(point) ))
+  calculatedYpixel= convert(Int32,round(GeometricalPredicates.gety(point) ))
+  sliceNumbConv= convert(Int32,round(GeometricalPredicates.getz(point) ))
+
+#
+# - 
+
+
+end
 
