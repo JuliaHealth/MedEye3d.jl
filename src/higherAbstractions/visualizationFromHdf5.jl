08c8bbde60d9e1840df39c986caae5f9483dd3e7
Jakub Mitura <jakub.mitura@gmail.com>
2023-10-30 12:37:20 +0100

    up
    Signed-off-by: Jakub Mitura <jakub.mitura@gmail.com>

--- src/higherAbstractions/visualizationFromHdf5.jl
+++ src/higherAbstractions/visualizationFromHdf5.jl
@@ -275,7 +275,7 @@ function getDefaultTextureSpec(dataTypeStr::String,maskName::String ,index::Int,
      )
  
   elseif(dataTypeStr=="contLabel") 
-    return TextureSpec{Float32}(
+    return TextureSpec{typp}(
       name = maskName,
       isContinuusMask=true,
       colorSet = [getSomeColor(listOfColorUsed),getSomeColor(listOfColorUsed)]
